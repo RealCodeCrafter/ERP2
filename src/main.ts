03c1f835a4bae2d42817631c537c99f6de894160
Vali <valireyimbergenov79@gmail.com>
2025-10-04 03:11:04 +0500

    update application.dto.create

--- src/main.ts
+++ src/main.ts
@@ -20,8 +20,9 @@ async function bootstrap() {
     origin: "*",
     credentials: true,
   });
+const port = process.env.PORT || 3000;
+await app.listen(port, "0.0.0.0");
 
-  await app.listen(3000, "0.0.0.0");
 }
 
 bootstrap();
